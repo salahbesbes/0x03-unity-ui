6056e230336290c375d02d7fad314d4b694bd10c
salahbesbes <salah_besbes@ymail.com>
2021-08-09 15:28:17 +0100

    Win/Lose Message,  Caroutine-> wait some time, create new scene Menu

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,33 +10,37 @@ public class PlayerController : MonoBehaviour
 	private int score = 0;
 	public int health = 5;
 
+	public Text scoreText;
+	public Text healthText;
+	public Image winLoseImage;
 
-	void OnTriggerEnter(Collider other)
+	private void OnTriggerEnter(Collider other)
 	{
-
 		switch (other.tag)
 		{
 			case "Pickup":
 				score++;
-				Debug.Log($"Score: {score}");
+				SetScoreText();
 				Destroy(other.gameObject); // destroy
-							   // other.gameObject.SetActive(false); // desActivate
+
 				break;
+
 			case "Trap":
 				health--;
-				Debug.Log($"Health: {health}");
+				SetHealthText();
 				break;
+
 			case "Goal":
-				Debug.Log("You win!");
+				DisplayWinPanel();
+				StartCoroutine(LoadScene(3));
 				break;
+
 			default:
 				return;
 		}
 	}
 
-
-
-	void FixedUpdate()
+	private void FixedUpdate()
 	{
 		float currentXPosition = Input.GetAxis("Horizontal");
 		float currentZPosition = Input.GetAxis("Vertical");
@@ -44,19 +50,55 @@ public class PlayerController : MonoBehaviour
 		Vector3 force = dir * speed * Time.deltaTime;
 
 		rb.AddForce(force);
-
-
 	}
 
-	void Update()
+	private void Update()
 	{
 		if (health == 0)
 		{
-			Debug.Log("Game Over!");
-			health = 5;
-			score = 0;
-			Scene scene = SceneManager.GetActiveScene();
-			SceneManager.LoadScene(scene.name);
+			DisplayLosePanel();
+			StartCoroutine(LoadScene(3));
 		}
+	}
+
+	private void SetScoreText()
+	{
+		scoreText.text = $"Score {score}";
+	}
+
+	private void SetHealthText()
+	{
+		healthText.text = $"Health: {health}";
+	}
+
+	private void DisplayWinPanel()
+	{
+		Text winLoseText = winLoseImage.GetComponentInChildren<Text>();
+		winLoseText.text = "You Win!";
+		winLoseText.color = Color.black;
+
+		winLoseImage.color = Color.green;
+
+		winLoseImage.gameObject.SetActive(true);
+	}
+
+	private void DisplayLosePanel()
+	{
+		Text winLoseText = winLoseImage.GetComponentInChildren<Text>();
+		winLoseText.text = "Game Over!";
+		winLoseText.color = Color.white;
+
+		winLoseImage.color = Color.red;
+
+		winLoseImage.gameObject.SetActive(true);
+	}
+
+	private IEnumerator LoadScene(float seconds)
+	{
+		yield return new WaitForSeconds(seconds);
+		health = 5;
+		score = 0;
+		Scene scene = SceneManager.GetActiveScene();
+		SceneManager.LoadScene(scene.name);
 	}
 }
